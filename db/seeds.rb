31a468f2518f07382005e4b3b7e182f82a789512
MPaulina <mpolina91@gmail.com>
2014-05-21 12:32:51 +0200

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times { |i| Gist.create(snippet: "Post #{i}", lang: "java", description: "abc") }
+10.times { |i| Gist.create(snippet: "int i;", lang: "C#", description: "integer - całkowitoliczbowa") }
+10.times { |i| Gist.create(snippet: "string name = 'Dziewczyny Rubiego';", lang: "C", description: "łańcuch znaków") }
+10.times { |i| Gist.create(snippet: "private void Function () {};", lang: "Java", description: "funckja, która nic nie zwraca") }
+10.times { |i| Gist.create(snippet: "float d;", lang: "C", description: "double - zmiennoprzecinkowa") }
+10.times { |i| Gist.create(snippet: "body { font-size: 15px; };", lang: "CSS", description: "rozmiar czcionki dla body") }
